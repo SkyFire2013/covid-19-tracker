91588423f258fb44c01cc94b4a67f366e2b2240a
Jonn Jasper Ejoc <ejocjonnjasper@gmail.com>
2020-05-13 20:34:15 +0800

    added build files

---
+++ build/precache-manifest.7947ced88576977447ad1b6df3778bc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1653d89735b0d82943fd56e6964085eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a7154dbd6666b6ee57f",
+    "url": "/static/css/main.0819272a.chunk.css"
+  },
+  {
+    "revision": "d06e3f29846ee299ca55",
+    "url": "/static/js/2.ba296825.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.ba296825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a7154dbd6666b6ee57f",
+    "url": "/static/js/main.cfb9297d.chunk.js"
+  },
+  {
+    "revision": "63f4e8b58fac8513d449",
+    "url": "/static/js/runtime-main.310cdbaa.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
